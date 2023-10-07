70ab2b6b80eeac949a4cdd9dd5604633c4b6623b
Amosky <amoroussel05@gmail.com>
2023-10-07 23:03:06 +0200

    camera movements

---
+++ LogicSandbox/Assets/Scripts/CameraMovements.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraMovements : MonoBehaviour
+{
+
+    public Camera maincamera;
+
+    public float mouseScrollSpeed = 1f;
+    public float mouseMoveSpeed = 1f;
+    public float SmoothZoomSpeed = 7f;
+    public float SmoothCenterSpeed = 7f;
+    public float MinZoomValue = 0.2f;
+    public float MaxZoomValue = 15f;
+
+    public bool followPlayer = true;
+    public GameObject Player;
+
+    private Vector3 dragOrigin;
+    public float Tozoomvalue;
+    public Vector3 ToCenter;
+
+    public int MouseButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincamera.orthographic = true;
+        Tozoomvalue = maincamera.orthographicSize;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (followPlayer)
+        {
+            ToCenter = Player.transform.position;
+        }
+
+        ApplyDrag();
+        CalculateZoom();
+        ApplyZoom();
+        ApplyCameraCenter();
+
+    }
+
+
+    void ApplyDrag()
+    {
+        if (Input.GetMouseButtonDown(MouseButton)) { 
+            dragOrigin = maincamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(MouseButton)) {
+            Vector3 difference = dragOrigin - maincamera.ScreenToWorldPoint(Input.mousePosition);
+            ToCenter = maincamera.transform.position + difference;
+        }
+    }
+
+
+    void CalculateZoom()
+    {
+        float deltazoomvalue = Input.mouseScrollDelta.y;
+
+        if (deltazoomvalue != 0f)
+        {
+
+            float newZvalue = Tozoomvalue;
+            newZvalue *= (float)System.Math.Pow((1 - 0.05d * mouseScrollSpeed), (double)deltazoomvalue);
+
+            Tozoomvalue = Mathf.Clamp(newZvalue, MinZoomValue, MaxZoomValue);
+        }
+    }
+
+
+    void ApplyZoom()
+    {
+        float orthographicSizeDifference = Tozoomvalue - maincamera.orthographicSize;
+        maincamera.orthographicSize += orthographicSizeDifference * SmoothZoomSpeed * Time.deltaTime;
+    }
+
+
+    void ApplyCameraCenter()
+    {
+        if (ToCenter != null)
+        {
+            Vector3 CentersDifference = ToCenter - maincamera.transform.position;
+            maincamera.transform.position += CentersDifference * SmoothCenterSpeed * Time.deltaTime;
+            maincamera.transform.position = new Vector3(maincamera.transform.position.x , maincamera.transform.position.y , -10f);
+
+        }
+    }
+
+}
